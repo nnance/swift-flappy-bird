f737d32e9ef392b5b473aca23d6476c4a5ef2db2
Nick Nance <nance.nick@gmail.com>
2020-10-04 18:45:38 -0700

    pure factory functions for bird and background

--- FlappyBird/GameScene.swift
+++ FlappyBird/GameScene.swift
@@ -6,47 +6,56 @@
 //
 
 import SpriteKit
-import GameplayKit
+
+func birdFactory(pos: CGPoint) -> SKSpriteNode {
+    let birdTexture = SKTexture(imageNamed: "flappy1.png")
+    let birdTexture2 = SKTexture(imageNamed: "flappy2.png")
+    
+    let animation = SKAction.animate(with: [birdTexture, birdTexture2], timePerFrame: 0.1)
+    let makeBirdFlap = SKAction.repeatForever(animation)
+
+    let bird = SKSpriteNode(texture: birdTexture)
+    bird.position = pos
+    
+    bird.run(makeBirdFlap)
+
+    return bird
+}
+
+func backgroundFactory(y: CGFloat, height: CGFloat) -> [SKSpriteNode] {
+    let bgTexture = SKTexture(imageNamed: "bg.png")
+
+    let bgVector = CGVector(dx: -bgTexture.size().width, dy: 0)
+    let moveBGAnimation = SKAction.move(by: bgVector, duration: 3)
+
+    let shiftVector = CGVector(dx: bgTexture.size().width, dy: 0)
+    let shiftBGAnimation = SKAction.move(by: shiftVector, duration: 0)
+
+    let moveBGForever = SKAction.repeatForever(SKAction.sequence([moveBGAnimation, shiftBGAnimation]))
+
+    var i: CGFloat = 0
+    var results: [SKSpriteNode] = []
+    
+    while i < 3 {
+        let bg = SKSpriteNode(texture: bgTexture)
+        bg.position = CGPoint(x: bgTexture.size().width * i, y: y)
+        bg.size.height = height
+        bg.zPosition = -1
+        bg.run(moveBGForever)
+        results.append(bg)
+
+        i += 1
+    }
+    return results
+}
 
 class GameScene: SKScene {
     
-    var bird = SKSpriteNode()
-    var bg = SKSpriteNode()
-    
     override func didMove(to view: SKView) {
-        let bgTexture = SKTexture(imageNamed: "bg.png")
-
-        let bgVector = CGVector(dx: -bgTexture.size().width, dy: 0)
-        let moveBGAnimation = SKAction.move(by: bgVector, duration: 3)
-
-        let shiftVector = CGVector(dx: bgTexture.size().width, dy: 0)
-        let shiftBGAnimation = SKAction.move(by: shiftVector, duration: 0)
-
-        let moveBGForever = SKAction.repeatForever(SKAction.sequence([moveBGAnimation, shiftBGAnimation]))
-
-        var i: CGFloat = 0
-        while i < 3 {
-            bg = SKSpriteNode(texture: bgTexture)
-            bg.position = CGPoint(x: bgTexture.size().width * i, y: self.frame.midY)
-            bg.size.height = self.frame.height
-            bg.zPosition = -1
-            bg.run(moveBGForever)
-            self.addChild(bg)
-
-            i += 1
-        }
+        let bgs = backgroundFactory(y: self.frame.midY, height: self.frame.height)
+        bgs.forEach { self.addChild($0) }
         
-        let birdTexture = SKTexture(imageNamed: "flappy1.png")
-        let birdTexture2 = SKTexture(imageNamed: "flappy2.png")
-        
-        let animation = SKAction.animate(with: [birdTexture, birdTexture2], timePerFrame: 0.1)
-        let makeBirdFlap = SKAction.repeatForever(animation)
-
-        bird = SKSpriteNode(texture: birdTexture)
-        bird.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
-        
-        bird.run(makeBirdFlap)
-        
+        let bird = birdFactory(pos: CGPoint(x: self.frame.midX, y: self.frame.midY))
         self.addChild(bird)
     }
     
